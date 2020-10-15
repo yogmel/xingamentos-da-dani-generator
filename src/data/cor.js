83659b1712787b9a3bb0bf5b4b1b9913d131c92f
Mellina Yonashiro <yonashiro.mellina@gmail.com>
2020-10-14 22:55:04 -0300

    Add data objects

---
+++ src/data/cor.js
@@ -0,0 +1,12 @@
+export default {
+    "branca": "num pinheiro",
+    "preta": "num mar de rola",
+    "vermelha": "na estrada",
+    "azul": "na sarjeta",
+    "cinza": "na puta que pariu",
+    "verde": "num canavial de rola",
+    "listrada": "na casa da sua mãe",
+    "florida": "onde o sol não bate",
+    "amarela": "na casa do caralho",
+    "outra": "en la concha de tu madre"
+}
---
+++ src/data/mes.js
@@ -0,0 +1,14 @@
+export default {
+    1: "pegando fogo",
+    2: "arrancando os cabelos do cu com a mão",
+    3: "nadando de costas",
+    4: "chupando um canavial de rola",
+    5: "se arrombando",
+    6: "explodindo o cu",
+    7: "tirando a calcinha pela boca",
+    8: "rasgando o cu com a unha",
+    9: "cheirando uma véia",
+    10: "comenda a própria merda",
+    11: "jogando no time dos sem camisa",
+    12: "dirigindo um caminhão com a teta de fora"
+}
